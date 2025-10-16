34ab5fdd0cfa776a937d2d86df2e714b8301a059
Kuba <jakubn@ntropy.pl>
2025-10-16 11:51:10 +0200

    feat: initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,17 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
+import clerk from '@clerk/astro';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), sitemap()],
-  adapter: vercel()
+  integrations: [react(), sitemap(), clerk()],
+  adapter: vercel(),
+  output: 'server',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
