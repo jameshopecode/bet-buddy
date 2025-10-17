2cec7c7f9b6d43aa4a01c6a1f5b648e34470d781
Damian Kisielewski <dkisielewski@gmail.com>
2025-10-17 14:08:55 +0200

    Implemented knowladge base and connected to agent

--- BetBuddy.Backend/Ai/KernelBuilder.cs
+++ BetBuddy.Backend/Ai/KernelBuilder.cs
@@ -22,44 +22,58 @@ public class KernelBuilder
             port: 6334,
             https: false
         );
+        builder.Plugins.AddFromType<FixturesVectorStore>("Fixtures");
         var kernel = builder.Build();
         var vectorStore = kernel.GetRequiredService<QdrantVectorStore>();
         var embeddingService = kernel.GetRequiredService<IEmbeddingGenerator<string, Embedding<float>>>();
-        builder.Plugins.AddFromObject(new FixturesVectorStore(vectorStore, embeddingService));
-        
-        
+
+        //Local seed for debug, real not here
+        //InitializeVectorStoreAsync(vectorStore, embeddingService).Wait();
         return kernel;
     }
     
      public async Task InitializeVectorStoreAsync(QdrantVectorStore vectorStore, IEmbeddingGenerator<string, Embedding<float>> embeddingService)
     {
             // Get the car collection
-            var qdrantCollection = vectorStore.GetCollection<Guid, Fixture>("Fixtures");;
+            var qdrantCollection = vectorStore.GetCollection<Guid, Fixture>("fixtures");;
             
             // Ensure the collection exists
             await qdrantCollection.EnsureCollectionExistsAsync();
 
             // Sample car inventory
-            var cars = new[]
+            var fixtures = new[]
             {
                 new Fixture()
                 {
                     Id = Guid.NewGuid(),
-                    IsAvailable = true,
-                    Description = "Reliable midsize sedan with excellent fuel economy, advanced safety features, and spacious interior perfect for daily commuting."
+                    MatchId = 00123989,
+                    Markets = new []{123,456,65,45,67},
+                    Date = "2025-11-12",
+                    Home = "Real",
+                    Away = "Barcelona",
+                    Description = "Match Real vs Barcenolna Date time: 2025-11-12 in La Liga competition."
                 },
-             
+                new Fixture()
+                {
+                    Id = Guid.NewGuid(),
+                    MatchId = 999866,
+                    Markets = new []{659,4489,02,8634,644},
+                    Date = "2025-11-12",
+                    Home = "Arsenal",
+                    Away = "City",
+                    Description = "Match Arsenal vs City Date time: 2025-11-14 in Premier League competition."
+                }
             };
 
             // Generate embeddings and upsert cars
-            foreach (var car in cars)
+            foreach (var car in fixtures)
             {
                 var embedding = await embeddingService.GenerateAsync(car.Description);
                 car.DescriptionEmbedding = embedding.Vector;
             }
 
-            await qdrantCollection.UpsertAsync(cars);
+            await qdrantCollection.UpsertAsync(fixtures);
             
-            Console.WriteLine($"Initialized car inventory with {cars.Length} vehicles.");
+            Console.WriteLine($"Initialized car inventory with {fixtures.Length} vehicles.");
     }
 }
