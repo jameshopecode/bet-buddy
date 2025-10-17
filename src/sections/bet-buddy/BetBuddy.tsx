e3abb1f2375118b0948d7cc15267bc5a60b15336
Kuba <jakubn@ntropy.pl>
2025-10-17 15:21:29 +0200

    feat: added initial bet buddy bubble

--- src/sections/bet-buddy/BetBuddy.tsx
+++ src/sections/bet-buddy/BetBuddy.tsx
@@ -1,11 +1,16 @@
 import type { FC } from 'react';
+import { LightningIcon } from 'src/components/icons/LightningIcon.tsx';
 
-interface IBetBuddyProps {
+type Props = {
+  userId: string;
+};
 
-}
-
-const BetBuddy: FC<IBetBuddyProps> = () => {
-  return null;
+const BetBuddy: FC<Props> = () => {
+  return (
+    <div className="bg bg-secondary hover:border-primary fixed right-4 bottom-4 cursor-pointer rounded-full p-7 transition-shadow hover:shadow-[3px_3px_10px_-0.5px_#16bdf9]">
+      <LightningIcon />
+    </div>
+  );
 };
 
 export default BetBuddy;
