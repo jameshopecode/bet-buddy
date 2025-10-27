18fbb54944c5285609349ebdc2d43c52cda4b54c
Damian Kisielewski <dkisielewski@gmail.com>
2025-10-27 14:05:14 +0100

    fix model id

--- BetBuddy.Backend/Ai/KernelBuilder.cs
+++ BetBuddy.Backend/Ai/KernelBuilder.cs
@@ -14,7 +14,7 @@ public class KernelBuilder
         var client = new HttpClient();
         client.BaseAddress = new Uri("http://ollama-server:11434");
         client.Timeout = TimeSpan.FromMinutes(3);
-        builder.AddOllamaChatCompletion("llama3.2:1b",client); // switch to gpt-oss:20b-cloud for faster, more accurate solution
+        builder.AddOllamaChatCompletion("gpt-oss:20b-cloud",client); // switch to gpt-oss:20b-cloud for faster, more accurate solution
         builder.AddOllamaEmbeddingGenerator("nomic-embed-text:v1.5",client);
 
         builder.Services.AddQdrantVectorStore(
